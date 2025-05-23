40ec20bdfeab3afa1ea5cefadf359b8000ffd056
prabuddha610 <prabuddha610@gmail.com>
2025-05-23 10:25:00 +0530

    Add Smallest of Arrays

---
+++ smallest_arrays.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int nums[]={5,15,10,-34,2,89};
+    int size = 6;
+
+    int smallest;
+
+    for(int i=0;i<size;i++){
+        if(nums[i] < smallest){
+            smallest = nums[i];
+        }
+    }
+
+    cout<<"Smallest = " << smallest << endl;
+    return 0;
+}
