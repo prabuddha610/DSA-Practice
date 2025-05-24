306b2db14badbfe939ce7df0ff88ba41beacb343
prabuddha610 <prabuddha610@gmail.com>
2025-05-24 14:32:49 +0530

    Add Maximum Sub Array

---
+++ maximumSubarray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n=5;
+    int arr[5]= {1,2,3,4,5};
+
+    for(int st=0; st<n; st++){
+        for(int end=st; end<n;end++){
+            for(int i=st; i<=end;i++){
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// 1 12 123 1234 12345
+// 2 23 234 2345
+// 3 34 345
+// 4 45
+// 5
