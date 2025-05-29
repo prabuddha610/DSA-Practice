00a94851a6c4eccfdc6eee1493533019495de95c
prabuddha610 <prabuddha610@gmail.com>
2025-05-29 11:22:01 +0530

    Add Pointer for Pointer

---
+++ pointerOfptr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int* ptr = &a;
+
+    int** parPtr = &ptr;
+
+    cout << &ptr << endl;
+    cout << parPtr << endl;
+    return 0;
+}
