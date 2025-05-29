c8fc135b189e1f718c5292f661d4c2129874a5e3
prabuddha610 <prabuddha610@gmail.com>
2025-05-29 12:58:22 +0530

    Add Pointer Arithmetic

---
+++ pointerArithmetic.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+
+    int a= 10;
+    int *ptr = &a;
+
+    cout << ptr << endl;
+    ptr = ptr +2; //2int => 8B
+    cout << ptr << endl; //+8
+    return 0;
+}
