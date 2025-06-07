07c6c0a6175f25bddba82641e4ac8edcd742f0d5
prabuddha610 <prabuddha610@gmail.com>
2025-06-07 21:01:11 +0530

    Add Bubble Sort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 void bubbleSort(int arr[], int n){ //O(n^2)
     for(int i=0;i<n-1;i++){
+        bool isSwap = false;
+
         for(int j=0; j<n-i-1;j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+        if(!isSwap){ // the array is already sorted
+            return;
         }
     }
 }
